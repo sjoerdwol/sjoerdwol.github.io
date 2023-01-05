774cd5909329b187216ea9632d497222d5f34baf
sjoerdwol <sjoerd@woltersonline.net>
2023-01-05 11:24:45 +0100

    filled skillData

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -2,9 +2,9 @@ export function InfoCard(props) {
   const content = props.content;
 
   return (
-    <div className=' text-center border-2 border-htmlgray-500 border-opacity-40 rounded-xl p-3 mb-5 last:m-0 2xl:basis-2/5 2xl:m-0' >
+    <div className=' text-center border-2 border-htmlgray-500 dark:border-beaublue-500 border-opacity-40 dark:border-opacity-60 rounded-xl p-3 mb-5 last:m-0 2xl:basis-2/5 2xl:m-0' >
       <h4 className='pb-4 font-semibold text-darkjunglegreen-500 dark:text-platinum-500 md:text-2xl'>{content.title}</h4>
-      <p className='break-words px-5 text-darkjunglegreen-500 dark:text-platinum-500 md:px-10 md:text-xl h-auto xl:px-20 xl:break-all '>{content.description}</p>
+      <p className='px-5 text-darkjunglegreen-500 dark:text-platinum-500 md:px-10 md:text-xl h-auto xl:px-16'>{content.description}</p>
       {content.icons && <div className='pt-6 flex flex-wrap justify-center text-2xl gap-4 text-darkjunglegreen-500 dark:text-platinum-500 md:text-5xl md:gap-8'>
         {content.icons.map(icon => { return icon })}
       </div>}
--- src/components/data/skillsData.js
+++ src/components/data/skillsData.js
@@ -4,17 +4,17 @@ import { SiCucumber, SiCypress, SiTailwindcss, SiSonarqube, SiJenkins } from 're
 export const skillsData = [
   {
     title: 'Frontend Web Development',
-    description: 'Cooler Text',
+    description: 'Since my studies I was interested in Frontend Web Development. I decided to follow this up with the Frontend Engineer Career Path on Codecademy. Even though I have not finished it yet, I have learned quite a lot already.',
     icons: [<FaHtml5 />, <FaCss3Alt />, <SiTailwindcss />, <FaJsSquare />, <FaReact />]
   },
   {
     title: 'Software Quality Assurance', 
-    description: 'text',
+    description: 'At my current job, I am a Software Engineer who is tasked with teaching Software Engineers on how to properly test your software. For this, I need expertise in static analysis, software testing and test automation.',
     icons: [<SiCypress />, <SiCucumber />, <SiSonarqube />]
   },
   {
     title: 'General Developer Knowledge', 
-    description: 'text',
+    description: 'During both my job and my hobby development, I use a lot of different development tools. This includes Git, GitHub aswell as Jenkins for CI/CD.',
     icons: [<FaGitAlt />, <FaGithub />, <FaNpm />, <SiJenkins />]
   },
 ];
