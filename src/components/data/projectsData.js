dec79aa3bef12ec34d4aa26c86b946ae86d29ac3
sjoerdwol <sjoerd@woltersonline.net>
2023-01-05 14:37:33 +0100

    filled projectData

--- src/components/data/projectsData.js
+++ src/components/data/projectsData.js
@@ -1,3 +1,10 @@
 export const projectsData = [
-  {title: 'Portfolio', description: ''}
+  {
+    title: 'Portfolio', 
+    description: 'This portfolio is my first proper project. Originally it was made using HTML, CSS and JavaScript only. Currently it is build on React and Tailwind. Even though this might sound over-engineered, I like using this portfolio as my playground to practice and learn skills.',
+  },
+  {
+    title: 'Web Development Cheat Sheet', 
+    description: 'Something I created during the Frontend Engineer Career Path is a cheat sheet for all topics around Frontend Web Development. I keep all my notes here and I will extend this cheat sheet on other topics while learning them. You can see it on my GitHub.',
+  }
 ];
