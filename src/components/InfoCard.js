70abd0745dd247f2ba93f1f878bddd21fb34dc96
sjoerdwol <sjoerd@woltersonline.net>
2023-01-05 09:51:34 +0100

    implemented flex cards for larger screens

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -2,9 +2,9 @@ export function InfoCard(props) {
   const content = props.content;
 
   return (
-    <div className=' text-center border-2 border-black rounded-xl p-3 mb-5 last:m-0' >
+    <div className=' text-center border-2 border-black rounded-xl p-3 mb-5 last:m-0 2xl:basis-2/5 2xl:m-0' >
       <h4 className='pb-4 font-semibold md:text-2xl'>{content.title}</h4>
-      <p className='break-words px-5 md:px-10 md:text-xl h-auto xl:px-20 2xl:px-28 3xl:px-36'>{content.description}</p>
+      <p className='break-words px-5 md:px-10 md:text-xl h-auto xl:px-20 xl:break-all '>{content.description}</p>
       {content.icons && <div className='pt-6 flex flex-wrap justify-center text-2xl gap-4 md:text-5xl md:gap-8'>
         {content.icons.map(icon => { return icon })}
       </div>}
--- src/components/data/skillsData.js
+++ src/components/data/skillsData.js
@@ -1,14 +1,20 @@
-import { FaHtml5 , FaCss3Alt , FaJsSquare , FaReact } from 'react-icons/fa';
-import { SiTailwindcss } from 'react-icons/si';
+import { FaHtml5 , FaCss3Alt , FaJsSquare , FaReact, FaGitAlt, FaGithub, FaNpm } from 'react-icons/fa';
+import { SiCucumber, SiCypress, SiTailwindcss, SiSonarqube, SiJenkins } from 'react-icons/si';
 
 export const skillsData = [
   {
     title: 'Frontend Web Development',
-    description: 'hahahahahahahahahahahahahahahahhaahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhaahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhaahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhaahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhaahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhaahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhaahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahhaahahahahahahahahahahahahahaha',
+    description: 'Cooler Text',
     icons: [<FaHtml5 />, <FaCss3Alt />, <SiTailwindcss />, <FaJsSquare />, <FaReact />]
   },
   {
     title: 'Software Quality Assurance', 
-    description: 'text'
-  }
+    description: 'text',
+    icons: [<SiCypress />, <SiCucumber />, <SiSonarqube />]
+  },
+  {
+    title: 'General Developer Knowledge', 
+    description: 'text',
+    icons: [<FaGitAlt />, <FaGithub />, <FaNpm />, <SiJenkins />]
+  },
 ];
