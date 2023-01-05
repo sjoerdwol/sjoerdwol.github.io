8602000f55c57cf186cca1704e3d9a1f92a46bc4
sjoerdwol <sjoerd@woltersonline.net>
2023-01-05 14:58:32 +0100

    added alts and titles to all icons

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,4 +1,4 @@
-import { AiFillLinkedin, AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillLinkedin, AiOutlineTwitter, AiFillGithub } from 'react-icons/ai';
 import picture from './data/images/sjoerd_picture.jpg';
 
 export function Information(props) {
@@ -7,8 +7,9 @@ export function Information(props) {
       <h2 className='text-3xl font-semibold text-darkjunglegreen-500 dark:text-platinum-500 md:text-5xl'>Sjoerd Wolters</h2>
       <h3 className='text-xl py-2 text-darkjunglegreen-500 dark:text-platinum-500 md:text-2xl md:py-5'>Software Engineer and Hobby-Developer</h3>
       <div className='flex text-4xl justify-center gap-3 py-2 md:text-6xl md:gap-4'>
-        <AiFillLinkedin className='text-darkjunglegreen-500 dark:text-platinum-500' />
-        <AiOutlineTwitter className='text-darkjunglegreen-500 dark:text-platinum-500' />
+        <a href='https://www.linkedin.com/in/sjoerdwol/'><AiFillLinkedin className='text-darkjunglegreen-500 dark:text-platinum-500' alt='LinkedIn' title='LinkedIn' /></a>
+        <a href='https://twitter.com/realwatsn'><AiOutlineTwitter className='text-darkjunglegreen-500 dark:text-platinum-500' alt='Twitter' title='Twitter' /></a>
+        <a href='https://github.com/sjoerdwol'><AiFillGithub className='text-darkjunglegreen-500 dark:text-platinum-500' alt='GitHub' title='GitHub' /></a>
       </div>
       <img src={picture} alt='Sjoerd Wolters' className='mx-auto rounded-full mt-5 -rotate-45 w-3/5 border-4 border-darkjunglegreen-500 dark:border-platinum-500 md:mt-12 lg:w-2/5 xl:w-1/3 2xl:w-1/4 3xl:w-1/5 4xl:w-1/6' />
     </div>
--- src/components/data/skillsData.js
+++ src/components/data/skillsData.js
@@ -5,16 +5,16 @@ export const skillsData = [
   {
     title: 'Frontend Web Development',
     description: 'Since my studies I was interested in Frontend Web Development. I decided to follow this up with the Frontend Engineer Career Path on Codecademy. Even though I have not finished it yet, I have learned quite a lot already.',
-    icons: [<FaHtml5 />, <FaCss3Alt />, <SiTailwindcss />, <FaJsSquare />, <FaReact />]
+    icons: [<FaHtml5 title='HTML5' alt='HTML5' />, <FaCss3Alt title='CSS3' alt='CSS3' />, <SiTailwindcss title='Tailwind' alt='Tailwind' />, <FaJsSquare title='JavaScript' alt='JavaScript' />, <FaReact title='React' alt='React' />]
   },
   {
     title: 'Software Quality Assurance', 
     description: 'At my current job, I am a Software Engineer who is tasked with teaching Software Engineers on how to properly test your software. For this, I need expertise in static analysis, software testing and test automation.',
-    icons: [<SiCypress />, <SiCucumber />, <SiSonarqube />]
+    icons: [<SiCypress title='Cypress' alt='Cypress' />, <SiCucumber title='Cucumber' alt='Cucumber' />, <SiSonarqube title='SonarQube' alt='SonarQube' />]
   },
   {
     title: 'General Developer Knowledge', 
     description: 'During both my job and my hobby development, I use a lot of different development tools. This includes Git, GitHub aswell as Jenkins for CI/CD.',
-    icons: [<FaGitAlt />, <FaGithub />, <FaNpm />, <SiJenkins />]
+    icons: [<FaGitAlt title='Git' alt='Git' />, <FaGithub title='GitHub' alt='GitHub' />, <FaNpm title='npm' alt='npm' />, <SiJenkins title='Jenkins' alt='Jenkins' />]
   },
 ];
